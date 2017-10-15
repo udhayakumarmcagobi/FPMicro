fdf895f99c0c16a878239678cbcbb10c013a2e46
shivajiraoml <shikari123@gmail.com>
2017-10-15 16:32:08 +0530

    Email services functionality added
    email service class added in Core project
    to be called in controller, method expects
    fromaddress, toaddress, subject and body

---
+++ FBMicor.Web.Application/Core/EmailService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FBMicor.Web.Application.Core
+{
+    public class EmailService
+    {
+        public bool SendEmail(string subject, string emailBody, string toAddresses, string fromAddress)
+        {
+            var mailMessage = new MailMessage(); //Email Message            
+            var SMTPClient = new SmtpClient(); //Email client
+            string sSMTPServer = WebConfigurationManager.AppSettings["SMTP"];
+            mailMessage.To.Add(new MailAddress(toAddresses)); //To Address
+            mailMessage.From = new MailAddress(fromAddress); //From address
+            mailMessage.Subject = subject;
+            //Message type is HTML
+            mailMessage.IsBodyHtml = true;
+            
+            //Assign body content
+            mailMessage.Body = emailBody;
+
+            SMTPClient = new SmtpClient(sSMTPServer);
+            SMTPClient.Send(mailMessage);
+            return true;
+        }
+    }
+}
