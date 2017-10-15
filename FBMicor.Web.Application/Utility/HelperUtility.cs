35645ef0d31f3162dbe90cc771e78bdab1177a2e
Udhaya <udhayakumarmcagobi@gmail.com>
2017-10-15 17:53:31 +0530

    logo changes

---
+++ FBMicor.Web.Application/Utility/HelperUtility.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FBMicor.Web.Application.Utility
+{
+    public class HelperUtility
+    {
+        public static string HostPath
+        {
+            get
+            {
+                return HttpContext.Current.Request.Url.GetLeftPart(UriPartial.Authority) + HttpRuntime.AppDomainAppVirtualPath;
+            }
+        }
+    }
+}
